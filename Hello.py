9106f569359a32ac9d7a9ba6c36e2a54bb9fdafe
Man <sagittariusda@gmail.com>
2024-03-04 21:52:08 -0600

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -4,7 +4,8 @@ from langchain.llms import OpenAI
 st.title('Quikstart App')
 
 openai_api_key = st.sidebar.text_input('OpenAI API Key')
-st.sidebar.text_area('爱发电页面', 'https://afdian.net/a/Kaptech')
+#st.sidebar.text_area('爱发电页面', 'https://afdian.net/a/Kaptech')
+st.sidebar.text_area('Man Wang', 'github.com/SagittariusDA')
 def generate_response(input_text):
 	llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
 	st.info(llm(input_text))
